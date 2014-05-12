4a53b2f9dc33bdf9240d5ee430ef77939b987768
uvbeenzaned <uvbeenzaned2x@gmail.com>
2014-05-11 22:14:15 -0500

    Accepted experimental patch from Captain-Chaos.

--- src/co/networkery/uvbeenzaned/NETFire/NETFireListener.java
+++ src/co/networkery/uvbeenzaned/NETFire/NETFireListener.java
@@ -1,80 +1,65 @@
 package co.networkery.uvbeenzaned.NETFire;
 
 import java.io.File;
+import java.util.Arrays;
 
-import org.bukkit.Material;
+import static org.bukkit.Material.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import static org.bukkit.block.BlockFace.*;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.block.BlockRedstoneEvent;
 
 public class NETFireListener implements Listener
 {
-	
+
     public File configFile;
     public File usersFile;
     public FileConfiguration config;
     public FileConfiguration users;
-	
-	@EventHandler
-	public void onPowerBlock(BlockRedstoneEvent event)
-	{
-		if(config.getBoolean("enabled"))
-		{
-			Block pblock = event.getBlock();
-			if(event.getNewCurrent() > 0)
-			{
-				if(pblock.getType() == Material.NETHERRACK)
-				{
-					if(config.getBoolean("remove-block-above"))
-					{
-						pblock.getRelative(BlockFace.UP).setType(Material.FIRE);
-					}
-					else
-					{
-						if(pblock.getRelative(BlockFace.UP).getType() == Material.AIR)
-						{
-							pblock.getRelative(BlockFace.UP).setType(Material.FIRE);
-						}
-					}
-				}
-			}
-			else
-			{
-				if(pblock.getRelative(BlockFace.UP).getType() == Material.FIRE)
-				{
-					pblock.getRelative(BlockFace.UP).setType(Material.AIR);
-				}
-			}
-		}
-	}
-	
-	@EventHandler
-	public void onBlockPlace(BlockPlaceEvent event)
-	{
-		if(config.getBoolean("enabled"))
-		{
-			Block newblock = event.getBlockPlaced();
-			if(newblock.getType() == Material.NETHERRACK)
-			{
-				if(newblock.getBlockPower() > 0)
-				{
-					if(config.getBoolean("remove-block-above"))
-					{
-						newblock.getRelative(BlockFace.UP).setType(Material.FIRE);
-					}
-					else
-					{
-						if(newblock.getRelative(BlockFace.UP).getType() == Material.AIR)
-						{
-							newblock.getRelative(BlockFace.UP).setType(Material.FIRE);
-						}
-					}
-				}
-			}
-		}
-	}
+
+    @EventHandler
+    public void onPowerBlock(BlockRedstoneEvent event)
+    {
+        if(config.getBoolean("enabled"))
+        {
+            Block pblock = event.getBlock();
+            if(pblock.getType() == REDSTONE_WIRE)
+            {
+                setAlight(pblock, event.getNewCurrent() > 0, 1);
+            }
+            else
+            {
+                setAlight(pblock, event.getNewCurrent() > 0, 0);
+            }
+        }
+    }
+
+    /**
+     * Kinda sorta emulates pre-1.7 redstone notification behaviour.
+     */
+    private void setAlight(Block block, boolean alight, int level)
+    {
+        for(BlockFace dir: Arrays.asList(WEST, NORTH, EAST, SOUTH))
+        {
+            Block relBlock = block.getRelative(dir);
+            if(relBlock.getType() == NETHERRACK)
+            {
+                if(alight && (config.getBoolean("remove-block-above") || relBlock.getRelative(UP).isEmpty()))
+                {
+                    relBlock.getRelative(UP).setType(FIRE);
+                }
+                else if((!alight) && relBlock.getRelative(UP).getType() == FIRE)
+                {
+                    relBlock.getRelative(UP).setType(AIR);
+                }
+                if (level > 0)
+                {
+                    setAlight(relBlock, alight, level - 1);
+                }
+            }
+        }
+    }
 }
